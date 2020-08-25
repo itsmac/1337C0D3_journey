4e88270a64dc4f1038705f4063dd111e2d9063ce
itsmac <saagarmac1198@gmail.com>
2020-08-26 00:33:40 +0530

    Update Power of Two.py

--- Power of Two.py
+++ Power of Two.py
@@ -6,12 +6,4 @@ Easy
 
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        ct = 0
-        if n == 0:
-            return False
-        while n:
-            n = n & n-1
-            ct+=1
-            if ct > 1:
-                return False
-        return True
+        return n>0 and not(n&n-1)
